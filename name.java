ee3910e75287a13c060857f4350b04849f308dee
lsj104 <75536654+lsj104@users.noreply.github.com>
2021-03-22 23:59:07 +0900

    ex1

---
+++ name.java
@@ -0,0 +1,19 @@
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+
+public class name extends JFrame {
+	public void paint(Graphics g) {
+		super.paint(g);
+		g.drawLine(100,100,180,100);
+		g.drawLine(140,140,140,100);
+		g.drawLine(200, 100, 200, 200);
+		g.drawLine(140,140,180,180);
+		g.drawLine(140,140,120,180);
+		g.drawLine(120,200,120,230);
+		g.drawLine(120,230,230,230);
+	
+	
+		
+}
+}
---
+++ nameDemo.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class nameDemo {
+
+	public static void main(String[] args) {
+		name n = new name();
+		n.setTitle("이름 한 글자 쓰기");
+		n.setSize(300,300);
+		n.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		n.setVisible(true);
+
+
+	}
+
+}
