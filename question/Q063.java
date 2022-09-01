1fe377b3cfe64e59eef5af95bb357ed474703f7b
Hailey Lim <lsuj104@naver.com>
2022-09-02 02:42:48 +0900

    assignment

---
+++ question/Q063.java
@@ -0,0 +1,24 @@
+package com.test.question;
+
+import java.util.Arrays;
+
+public class Q063 {
+    public static void main(String[] args) {
+        //배열: 덤프
+
+        int[] list = new int[4];
+
+        list[0] = 10;
+        list[1] = 20;
+        list[2] = 30;
+        list[3] = 40;
+
+        String result = dump(list);
+        System.out.printf("nums = %s\n", result);
+    }
+
+    private static String dump(int[] list) {
+        String arrayToString = Arrays.toString(list);
+        return arrayToString;
+    }
+}
